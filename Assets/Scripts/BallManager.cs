7d20d512b7a05dc602993beb9a82da3a367252ee
ra-men <ramenda1217@gmail.com>
2020-04-16 21:29:30 +0900

    P119まで実装

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //ボールがなにかに衝突
+    void OnCollisionEnter2D (Collision2D coll) {
+        if (coll.gameObject.tag =="OutArea") {
+            //アウトエリアに衝突
+            //ゲームマネージャーを取得
+            GameObject gameManager = GameObject.Find ("GameManager");
+            //リトライ
+            gameManager.GetComponent<GameManager> ().PushRetryButton ();
+            
+            
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int StageNo;     //ステージナンバー
+    public bool isBallMoving;    //ボールが移動中か
+
+    public GameObject ballPrefab;   //ボールプレハブ
+    public GameObject ball;         //ボールオブジェクト
+
+    public GameObject goButton;     //ボタン：開始時
+    public GameObject retryButton;  //ボタン：リトライ
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        retryButton.SetActive(false);   //リトライボタン非表示
+        isBallMoving = false;           //ボールは移動中ではない    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Goボタンを押した
+    public void PushGoButton(){
+        //ボールの重力有効化
+        Rigidbody2D rd = ball.GetComponent<Rigidbody2D>();
+        rd.isKinematic = false;
+
+        retryButton.SetActive(true);
+        goButton.SetActive(false);
+        isBallMoving = true;
+    }
+
+      //Retryボタンを押した
+    public void PushRetryButton(){
+       Destroy (ball);
+
+       ball = (GameObject)Instantiate (ballPrefab);
+
+        retryButton.SetActive(false);
+        goButton.SetActive(true);
+        isBallMoving = false;
+    }
+}
